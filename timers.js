759a5c4ff75559f90b4123d2e2047eb9feb12b57
vadymdk <vadim.dk81@gmail.com>
2019-04-29 22:56:55 +0300

    firstcommit

---
+++ timers.js
@@ -0,0 +1,67 @@
+
+//1 Таймер должен получать время обратного отсчёта в конструкторе
+//2 Должен иметь кнопки старт(запускает) и стоп(ставит паузу).
+//3 Должен иметь в конструторе параметр для автоматического запуска(true/false). См на видео пример. 1ый таймер по кнопке запускается, а 2ой автоматически
+//4 Длина полоски уменьшается с движением таймера(анимацию советую делать потом отдельно, она в этом задании НЕОБЯЗАТЕЛЬНА)
+//Дополнительно: добавьте параметр в конструктор, который позволит передать интервал обновления таймера. Например раз в 1 секунду/100 мс/2с/10с. Обратите внимание на видео – нижний таймер меняется каждые 2 секунды.
+
+
+
+
+
+const container = document.querySelector("#timers-container");
+
+const TYPES = {
+     timerfirst: 10000,
+    timersecond: 99.3 *60000
+} 
+
+class Timer {
+    constructor(){
+        this.distance = 59000;
+//        this.type = type in TYPES ? type : "foo";
+        this.render();
+    }
+     
+    createTimer(){
+        let xTime = new Date("Jan 5, 2019 15:37:00").getTime();
+    let countDownDate = xTime + this.distance;
+  this.interval = setInterval(() =>{
+        xTime +=1000;
+      let diff = countDownDate - xTime;
+       let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        
+  // Display the result in the element with id="demo"
+  document.getElementById("timers").innerHTML =  minutes + ": " + seconds;
+
+  // If the count down is finished, write some text 
+  if (diff <= 0) {
+      clearInterval(this.interval);
+      document.getElementById("timers").innerHTML = "01"+":"+"00";
+  }
+}, 1000);
+        
+        
+    }
+    
+    pauseInterval(){
+        clearInterval(this.interval);
+    }
+    continueInterval(){
+        this.createTimer();
+    }
+    
+    render(){
+        this.timer = document.createElement("div");
+        this.timer.classList.add("counter");
+        this.timer.classList.add("this.type");
+        this.timer.append(this.createTimer());
+        
+        container.append(this.timer);
+        this.timer.addEventListener("mouseenter", this.pauseInterval.bind(this));
+        this.timer.addEventListener("mouseleave", this.continueInterval.bind(this));
+    }
+}
+    
+ new Timer( );   
